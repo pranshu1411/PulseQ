266904d6e9752d7ad393c8b86e1b2b9aac475758
Pranshu Gupta <pranshu.141104@gmail.com>
2026-06-17 16:52:14 +0530

    worker module infra

--- backend/apps/worker/src/worker.module.ts
+++ backend/apps/worker/src/worker.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { WorkerController } from './worker.controller';
 import { WorkerService } from './worker.service';
+import { BullModule } from '@nestjs/bullmq';
+import { PrismaModule } from '@app/prisma';
+import { ImageProcessor } from './image.processor';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST || 'localhost',
+        port: 6379,
+      },
+    }),
+  ],
   controllers: [WorkerController],
-  providers: [WorkerService],
+  providers: [WorkerService, ImageProcessor],
 })
 export class WorkerModule {}
